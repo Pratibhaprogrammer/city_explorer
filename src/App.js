054cd950d495b544ce97b4691b6562c16dfdd300
Jacob Holmer <yacub90@gmail.com>
2021-03-23 18:17:21 -0700

    error handling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Forcast from './forcast.js';
+import Error from './error.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,35 +11,29 @@ class App extends React.Component {
       location: {},
       citySearch: '',
       findImg: '',
-      displayResults: false
+      displayResults: false,
+      displayError: false,
+      error: {}
     }
 
   }
   LocationInfo = async (e) => {
     e.preventDefault();
     console.log(this.state.citySearch);
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MYKEY}&q=${this.state.citySearch}&format=json`;
-    const location = await axios.get(url).catch(function (error) {
-      if (error.response) {
-        // console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-    });
-    const locationArray = location.data;
-
-
-    this.setState({
-      location: locationArray[0],
-      displayResults: true,
-      findImg: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_MYKEY}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`
-    });
-  }
+    try{
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MYKEY}&q=${this.state.citySearch}&format=json`;
+      const location = await axios.get(url);
+      const locationArray = location.data;
+      this.setState({
+        location: locationArray[0],
+        displayResults: true,
+        findImg: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_MYKEY}&center=${locationArray[0].lat},${locationArray[0].lon}&zoom=13`
+      });
+    }catch(error){
+      console.log(error);
+      this.setState({displayError: true, error: error});
+    }
+    }
 
   render() {
     console.log('state', this.state)
@@ -62,9 +57,17 @@ class App extends React.Component {
               <Card.Text>
                 {this.state.location.lon}
               </Card.Text>
+              <Forcast></Forcast>
             </Card.Body>
           </Card>
           
+        }
+        {this.state.displayError && 
+          <>
+            <Error handleError = {this.state.error}/>
+
+            
+          </>
         }
       </>
     )
